8e81369169cfd47c728129e1610a657e02fd6665
Anthony Urbina <aurbinava002@gmail.com>
2022-08-29 17:20:34 +0000

    completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,22 @@
+function run() {
+  var fullName = 'Anthony Urbina';
+  console.log('value of fullName', fullName);
+  var isCool = true;
+  console.log('value of isCool', isCool);
+  var totalPets = 2;
+  console.log('value of totalPets', totalPets);
+
+  console.log('typeof fullName', typeof fullName);
+  console.log('typeof isCool', typeof isCool);
+  console.log('typeof totalPets', typeof totalPets);
+
+  var unknown = null;
+  console.log('value of unknown', unknown);
+  console.log('typeof unknown', typeof unknown);
+
+  var never;
+  console.log(never);
+  console.log('typeof never', typeof never);
+}
+
+run();
