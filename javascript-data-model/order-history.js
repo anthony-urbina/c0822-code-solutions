1712a219816fbd6264de94720d31a94eb7c88bb6
Anthony Urbina <aurbinava002@gmail.com>
2022-08-30 17:04:39 +0000

    completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,19 @@
+var orderHistory = [
+  {
+    jsBook: [
+      {
+        itemName: 'Javascript for impatient programmers',
+        itemDetails: [
+          { author: 'Rauschmayer, Dr. Axel' }
+        ],
+        orderPlaced: 'August 4 2020',
+        delivered: 'Aug 8,2020',
+        shipTo: 'JS Masher',
+        orderNumber: '#114-3941689-8772232',
+        total: '34.00',
+        beforeTax: '31.55',
+        returnWindow: 'Sep 7, 2020'
+      }
+    ]
+  }
+];
