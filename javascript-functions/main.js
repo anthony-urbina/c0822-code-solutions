d7d8fd207af0141838e92c80743da63117ba1d08
Anthony Urbina <aurbinava002@gmail.com>
2022-08-30 19:37:50 +0000

    added return statements

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -1,25 +1,40 @@
 /* eslint-disable no-unused-vars */
 function convertMinutesToSeconds(minutes) {
-  var seconds = minutes * 60;
-  console.log('seconds:', seconds);
+  var sum = minutes * 60;
+  return sum;
 }
+
+var seconds = convertMinutesToSeconds(5);
+console.log('seconds:', seconds);
 
 function greet(name) {
   var hey = 'Hey, ' + name;
-  console.log('greeting:', hey);
+  return hey;
 }
+
+var heyResult = greet('Beavis');
+console.log('greeting:', heyResult);
 
 function getArea(width, height) {
   var result = width * height;
-  console.log('area:', result);
+  return result;
 }
+
+var getAreaResult = getArea(2, 4);
+console.log('area:', getAreaResult);
 
 function getFirstName(person) {
-  var fetch = person.firstName;
-  console.log(' person object first name:', fetch);
+  var fetchFirstName = person.firstName;
+  return fetchFirstName;
 }
 
+var getFirstNameResult = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+console.log('person object first name:', getFirstNameResult);
+
 function getLastElement(array) {
-  var fetch = array[array.length - 1];
-  console.log('last element of array:', fetch);
+  var fetchLastElement = array[array.length - 1];
+  return fetchLastElement;
 }
+
+var getLastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('last element of array:', getLastElementResult);
