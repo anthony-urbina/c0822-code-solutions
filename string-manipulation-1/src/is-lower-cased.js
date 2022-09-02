a24835d4a786567eccbb86c977fccf2c747bd901
Anthony Urbina <aurbinava002@gmail.com>
2022-09-02 17:00:01 +0000

    completed string-manipulation-1 exercise

--- string-manipulation-1/src/is-lower-cased.js
+++ string-manipulation-1/src/is-lower-cased.js
@@ -1,1 +1,17 @@
 /* exported isLowerCased */
+/*
+create a function that returns a boolean indicating whether or not the words in a string are lowercase
+go through each letter in the string
+if you find a letter that is not equal to the lowercase version of itself
+stop and return false
+if nothing is found, at the end - return true
+*/
+
+function isLowerCased(word) {
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] !== word[i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+}
--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,1 +1,17 @@
 /* exported isUpperCased */
+/*
+create a function that returns a boolean indicating whether or not the words in a string are uppercase
+go through each letter in the string
+if you find a letter that is not equal to the uppercase version of itself
+stop and return false
+if nothing is found, at the end - return true
+*/
+
+function isUpperCased(word) {
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] !== word[i].toUpperCase()) {
+      return false;
+    }
+  }
+  return true;
+}
