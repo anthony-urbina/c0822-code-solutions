6bbb0ecd344c37f262233f865f8028701bd2673e
Anthony Urbina <aurbinava002@gmail.com>
2022-09-19 23:36:34 +0000

    completed javascript-constructors exercise

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var obj = new Object();
+console.log('value of obj', obj);
+console.log('typeof obj', typeof obj);
+
+var arr = new Array();
+console.log('value of arr', arr);
+console.log('typeof arr', typeof arr);
+
+var num = new Number();
+console.log('value of num', num);
+console.log('typeof num', typeof num);
+
+var bool = new Boolean();
+console.log('value of bool', bool);
+console.log('typeof bool', typeof bool);
+
+var str = new String();
+console.log('value of str', str);
+console.log('typeof str', typeof str);
