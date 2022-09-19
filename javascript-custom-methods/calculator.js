920c1bfcd16cea7f009a1b0c34c5342d76856c18
Anthony Urbina <aurbinava002@gmail.com>
2022-09-19 16:37:38 +0000

    completed javascript-custom-methods exercise

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,31 @@
+/* exported calculator */
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return Math.pow(x, 2);
+  },
+  sumAll: function (numbers) {
+    var numberBank = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      numberBank += numbers[i];
+    } return numberBank;
+  },
+  getAverage: function (numbers) {
+    var numberBank = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      numberBank += numbers[i];
+      var numbersAverage = numberBank / numbers.length;
+    } return numbersAverage;
+  }
+};
