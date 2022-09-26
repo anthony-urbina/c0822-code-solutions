e5ee561a83c76dc2535b7ed3d663e2a66b5d0218
Anthony Urbina <aurbinava002@gmail.com>
2022-09-26 15:49:24 +0000

    saving progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -13,23 +13,59 @@ if not return false
 this doesnt work because it doesn't remove newSecondString[i] that matches newFirstString[i]
 this cant happen, it throws the whole thing off
 so how do i get rid of a character when it matches
+
+if a word has x occurences of a letter
+but that x occurences of that letter is not present in the other word
+then it should return false
+
+how would i check that string have the same ammount of a certain character
+if first string at i is equal to any of the letters that were already pushed then it should skip
+
+create a storage container
+compare the first letter of string1 to every letter of string2
+if its a match and not already in the container then push it to the container and move on to the next character in string 2
+but if it does match a letter in the container you need to skip that character in the second string and keep searching the rest of it
+
+but then wont the same thing happen when you reach the letter again
+
+i can delete the letter
+save it's index
+then add it back after
 */
 
+// function isAnagram(firstString, secondString) {
+//   var storage = '';
+//   var newSecondString = secondString.replace(/ /g, '');
+//   var newFirstString = firstString.replace(/ /g, '');
+
+//   for (var i = 0; i < newFirstString.length; i++) {
+//     for (var j = 0; j <= newSecondString.length; j++) {
+//       if (newFirstString[i] === newSecondString[j] && storage.includes(newFirstString[i])) {
+//         storage = storage.concat(newFirstString[i]);
+//         newSecondString[i].replace('');
+//         break;
+//       } else if (newFirstString[i] === newSecondString[j] && !storage.includes(newFirstString[i])) {
+//         storage = storage.concat(newFirstString[i]);
+//         newSecondString[i].replace('');
+//         break;
+//       }
+//     }
+//   }
+//   if (storage === newFirstString) {
+//     return true;
+//   } else { return false; }
+// }
+
 function isAnagram(firstString, secondString) {
-  var storage = '';
+  var container = '';
   var newSecondString = secondString.replace(/ /g, '');
   var newFirstString = firstString.replace(/ /g, '');
-
   for (var i = 0; i < newFirstString.length; i++) {
-    for (var j = 0; j < newSecondString.length; j++) {
-      if (newFirstString[i] === newSecondString[j]) {
-        storage = storage.concat(newFirstString[i]);
-        newSecondString[i].replace('');
-        break;
-      }
+    if (newFirstString.includes(newSecondString[i]) && newFirstString.length === newSecondString.length) {
+      container = newFirstString;
     }
   }
-  if (storage === newFirstString) {
+  if (container === newFirstString) {
     return true;
   } else { return false; }
 }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,50 @@
 /* exported reverseWords */
+/*
+create a container
+iterate through the string
+any time you reach a space remove the characters before it including the space and assign them to a variable
+then you iterate through all the characters in that string starting from second to last
+send each to yet a NEW variable
+then you add a space to it
+and push it to the storage container
+but if you reach reach the last char in the string
+you must add everything in the string to a variable
+
+create a container
+iterate through the string
+when you reach a space i want to pause
+then i want to iterate through all of the chars before the space
+and starting from the last char before the space,
+i want to begin sending each char to the container
+then when there are no more chars to send i want to send a space to the container
+
+but if i reach the last char in the string,
+starting from the last char, i want to retroactively add characters to the container until i reach a space.
+
+when done, return the value of the container
+*/
+
+function reverseWords(string) {
+  var container = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      for (var j = 0; j <= i; j++) {
+        if (j === container.length) {
+          container = container.concat(' ');
+          break;
+        }
+        if (string[j] === ' ') {
+          break;
+        }
+        container = container.concat(string[i - j - 1]);
+      }
+    } else if (i === string.length - 1) {
+      for (var k = 0; k < string.length; k++) {
+        if (string[string.length - 1 - k] !== ' ') {
+          container = container.concat(string[string.length - 1 - k]);
+        }
+      }
+    }
+  }
+  return container;
+}
