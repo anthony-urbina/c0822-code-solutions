e054a4bf1483ce77986f2dda088a074927ac362e
Anthony Urbina <aurbinava002@gmail.com>
2022-10-26 01:11:17 +0000

    completed react-stopwatch-component exercise

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 
 class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: 0, running: false, icon: 'fa-solid fa-play' };
+    this.startTick = this.startTick.bind(this);
+  }
+
+  startTick() {
+    let intervalID;
+    if (!this.state.running) {
+      intervalID = setInterval(() => {
+        this.setState({ time: this.state.time + 1, running: true, icon: 'fa-solid fa-pause', intervalID });
+      }, 1000);
+    }
+    this.stopTick(this.state.intervalID);
+  }
+
+  stopTick(interval) {
+    clearInterval(interval);
+    this.setState({ running: false, icon: 'fa-solid fa-play' });
+  }
+
   render() {
     return (
       <div className='container'>
-        <div className='frame'>
-          <p>0</p>
+        <div className='row'>
+          <div className='frame'>
+            <p>{this.state.time}</p>
+          </div>
         </div>
-        <i className="fa-solid fa-play"></i>
+        <div className='row'>
+          <i onClick={this.startTick} className={this.state.icon}></i>
+        </div>
       </div>
     );
   }
