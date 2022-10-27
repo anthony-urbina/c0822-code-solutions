e43e5db9de1aaaf98865b45086642e31542080af
Anthony Urbina <aurbinava002@gmail.com>
2022-10-27 18:22:22 +0000

    saving progress, all packages and scripts have been loaded

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Accordian from './accordian';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<Accordian />);
