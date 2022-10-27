e2ba545e49dae57fa170fbbfc34cab580143a083
Anthony Urbina <aurbinava002@gmail.com>
2022-10-27 22:04:02 +0000

    completed fetch-in-react exercise

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ users, isLoading: false }))
+      .catch(err => {
+        // eslint-disable-next-line no-console
+        err && console.log('whoops, there was an error');
+      });
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
