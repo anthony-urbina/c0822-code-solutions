a5e55faf6dffb7e28e594acc2412ea05886a6560
Anthony Urbina <aurbinava002@gmail.com>
2022-10-29 21:54:59 +0000

    replaced object in the array instead of modifying it's properties

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -117,7 +117,7 @@ export default class App extends React.Component {
       })
       .then(data => {
         const todosCopy = [...this.state.todos];
-        todosCopy[toggledTodoIndex].isCompleted = todo.isCompleted;
+        todosCopy[toggledTodoIndex] = data;
         this.setState({ todos: todosCopy });
       });
 
