428289090ce7b81440eca10e47ac76894d1d81ca
Anthony Urbina <aurbinava002@gmail.com>
2022-10-31 20:57:14 +0000

    resolved error handling issues in argon2 promise chain

--- api-authentication/server/index.js
+++ api-authentication/server/index.js
@@ -75,7 +75,7 @@ app.post('/api/auth/sign-in', (req, res, next) => {
 
   db.query(sql, params)
     .then(result => {
-      if (!result) {
+      if (!result.rows[0]) {
         throw new ClientError(401, 'invalid login');
       } else {
         argon2.verify(result.rows[0].hashedPassword, password)
