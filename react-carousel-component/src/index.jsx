1a50ca2c225fe43168d2e74ba1175aa68193d7a4
Anthony Urbina <aurbinava002@gmail.com>
2022-11-01 18:41:38 +0000

    turned prop into an array of objects instead of just one giant object

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
-const pokemon = {
-  bulbasaur: 'images/001.png',
-  charmander: 'images/004.png',
-  pikachu: 'images/007.png',
-  squirtle: 'images/025.png',
-  jigglypuff: 'images/039.png'
-};
+const pokemon = [
+  { name: 'bulbasaur', src: 'images/001.png' },
+  { name: 'charmander', src: 'images/004.png' },
+  { name: 'squirtle', src: 'images/007.png' },
+  { name: 'squirtle', src: 'images/025.png' },
+  { name: 'jigglypuff', src: 'images/039.png' }
+];
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
