91d7271ce82625f4660ca7d7189011f4d7abc236
Anthony Urbina <aurbinava002@gmail.com>
2022-11-22 00:08:33 +0000

    made revisions

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -1,44 +1,44 @@
 /* exported uniqueQuadratic, uniqueLinear */
 
 function uniqueLinear(words) {
-  const seen = {};                  // 1 * 1 = O(1)
+  const seen = {};                  // 1 * 1 = O(1) / constant time
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= O(n)
+    i < words.length;               // 2 * n = O(2n) ~= O(n) // linear time
     i++                             // 2 * n = O(2n) ~= O(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n) ~= O(n)
+    const word = words[i];          // 2 * n = O(2n) ~= O(n) // linear time
     if (!seen[word]) {              // 3 * n = O(3n) ~= O(n)
-      seen[word] = true;            // 2 * 1 = O(2)
-      unique[unique.length] = word; // 3 * 1 = O(3)
+      seen[word] = true;            // 2 * n = O(2n) ~= O(n)
+      unique[unique.length] = word; // 3 * n = O(3)
     }
   }
   return unique;                    // 1 * 1 = O(1)
 } // Big O Notation for uniqueLinear: O(n)
 
 function uniqueQuadratic(words) {
-  const unique = [];                // 1 * 1 = O(1)
+  const unique = [];                // 1 * 1 = O(1) // constant time
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 1 * n = O(1n) ~= O(n)
-    i++                             // 2 * n = O(2n) ~= O(n)
+    i < words.length;               // 2 * n = O(2n) ~= O(n) // linear time
+    i++                             // 2 * n = O(2n) ~= O(n) // linear time
   ) {
-    const word = words[i];          // 2 * n = O(2n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 2 * n = O(2n) ~= O(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * n x n = O(1n^2) ~= O(n^2)
-      c < i;                        // 1 * 1 = O(1)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n x n = O(n^2)
       c++                           // 2 * n x n = O(2n^2) ~= O(n)
     ) {
       const comparing = words[c];   // 2 * n x n = O(2n^2) ~= O(n^2)
-      if (comparing === word) {     // 1 * 1 = O(1)
-        isUnique = false;           // 1 * 1 = O(1)
+      if (comparing === word) {     // 2 * n x n = O(2n^2) ~= O(n^2)
+        isUnique = false;           // 1 * n x n = O(n^2) // quadratic
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * 1 = O(3)
+    if (isUnique) {                 // 1 * n = O(n) // linear time
+      unique[unique.length] = word; // 3 * n = O(3n) ~= O(n) // linear time
     }
   }
-  return unique;                    // 1 * 1 = O(n^2)
-} // Big O Notation for uniqueQuadratic: O(?)
+  return unique;                    // 1 * 1 = O(1)
+} // Big O Notation for uniqueQuadratic: O(n^2)
