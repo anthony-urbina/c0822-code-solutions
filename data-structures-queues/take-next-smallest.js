a513edc89708c789cf67cad4714aa0d99dbfea88
Anthony Urbina <aurbinava002@gmail.com>
2022-11-24 02:15:05 +0000

    completed data-structures-queues exercise

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,18 +1,18 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
-  if (typeof queue.peek() !== 'undefined') {
-    let removed = queue.dequeue();
+  if (typeof queue.peek() === 'undefined') return;
+  let removed = queue.dequeue();
 
-    if (typeof queue.peek() === 'undefined') {
-      return removed;
-    }
-    if (removed <= queue.peek()) {
-      return removed;
-    }
-    while (removed > queue.peek()) {
-      queue.enqueue(removed);
-      removed = queue.dequeue();
-    }
+  if (typeof queue.peek() === 'undefined') { return removed; }
+
+  if (removed <= queue.peek()) { return removed; }
+
+  while (removed > queue.peek()) {
+    queue.enqueue(removed);
+    removed = queue.dequeue();
+
+    if (removed <= queue.peek()) { return removed; }
   }
+
 }
